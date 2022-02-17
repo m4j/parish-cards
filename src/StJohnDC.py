9b5dba8b3d07a7d4f651c486e09f765c975b4844
Maxim Agapov <m4j@outlook.com>
2022-02-17 13:03:36 -0500

    Python script printing member card

---
+++ src/StJohnDC.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+def connect(database = '../../StJohnDC.db'):
+    conn = sqlite3.connect(database)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+sql_cards_by_name = """select * from Cards C
+            where C.[Member Last] like :name OR
+                  C.[Member First] like :name OR
+                  C.[Member First Other] like :name OR
+                  C.[Member Middle] like :name OR
+                  C.[Member Maiden] like :name OR
+                  C.[RU Member Last] like :name OR
+                  C.[RU Member Maiden] like :name OR
+                  C.[RU Member First] like :name OR
+                  C.[RU Member Patronymic] like :name
+             order by [Member Last], [Member First]"""
+
+sql_dues_by_member = """select * from Payments_Dues
+            where [Member Last] like :lname AND
+                  [Member First] like :fname
+             order by [Paid From], [Paid Through]"""
