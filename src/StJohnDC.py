89ba9729c82f932e2d5e1dff6d794e69b91adca7
Maxim Agapov <maxim@maxa.ga>
2022-04-06 10:56:17 -0400

    Member class; moving some common functions to StJohnDC module

--- src/StJohnDC.py
+++ src/StJohnDC.py
@@ -27,3 +27,28 @@ table_cell_width = 11
 months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
 month_numbers = ['01','02','03','04','05','06','07','08','09','10','11','12']
 months_dict = { number : months[int(number)-1] for number in month_numbers }
+
+def printErrorIncorrect():
+    print('--------------------')
+    print('Incorrect, try again')
+    print('--------------------')
+
+def pickByIndex(rows):
+    count = len(rows)
+    if count == 0:
+        return None
+    if count == 1:
+        return 0
+    while True:
+        for i, row in enumerate(rows):
+            print('{}) {}'.format(i+1, row))
+        try:
+            selected = int(input('Pick one from the list:'))
+        except ValueError:
+            printErrorIncorrect()
+            continue
+        if selected-1 not in range(0, count):
+            printErrorIncorrect()
+            continue
+        return selected-1
+
