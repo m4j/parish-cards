b034cafd224dbd38e198d751fa6a84b73d2e6744
Maxim Agapov <m4j@outlook.com>
2025-03-13 17:20:12 -0400

    Search form enhancements to include clear button

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
-    search_term = StringField('Type word to search, like “irina”, or “Иван”. Word fragments work too, like “mar” or “joh”', validators=[DataRequired()], render_kw={'autofocus': True})
-    submit = SubmitField('Search')
+    search_term = StringField('Type word to search, like “irina”, or “Иван”. Word fragments work too, like “mar” or “joh”', render_kw={'autofocus': True})
+    submit_search = SubmitField('Search')
 
